2665090453cd208817e4b974b394dae5cf28e5d1
shwetapurushe <shwetapurushe@gmail.com>
2015-05-11 08:59:14 -0400

    d3 & router Bower

--- WeaveAnalyst/app/src/app.js
+++ WeaveAnalyst/app/src/app.js
@@ -25,104 +25,104 @@
 //                                 'aws.QueryHandlerModule'
 //                               ]); 
 
-var weave_Analyst = angular.module('weaveAnalyst',[]);
+var weave_Analyst = angular.module('weaveAnalyst',['ui.router']);
 
-//weave_Analyst.run(['$rootScope', function($rootScope){
-//	$rootScope.$safeApply = function(fn, $scope) {
-//			if($scope == undefined){
-//				$scope = $rootScope;
-//			}
-//			fn = fn || function() {};
-//			if ( !$scope.$$phase ) {
-//        	$scope.$apply( fn );
-//    	}
-//    	else {
-//        	fn();
-//    	}
-//	};
-//}])
-//.config(function($stateProvider, $urlRouterProvider, $parseProvider) {
-//	
-//	$parseProvider.unwrapPromises(true);
-//	
-//	$urlRouterProvider.otherwise('/index');
-//	
-//	$stateProvider
-//		.state('index', {
-//			url : '/index'
-//		})
-//		.state('metadata', {
-//			url:'/metadata',
-//			templateUrl : 'src/configure/metadata/metadataManager.html',
-//			controller: 'MetadataManagerCtrl',
-//			data : {
-//				activetab : 'metadata'
-//			}
-//		})
-//	    .state('script_management', {
-//	    	url:'/scripts',
-//	    	templateUrl : 'src/configure/script/scriptManager.html',
-//	    	controller : 'ScriptManagerCtrl',
-//	    	data:{
-//	    		activetab : 'script_management'
-//	    	}
-//	    })
-//	    .state('analysis', {
-//	    	url:'/analysis',
-//	    	templateUrl : 'src/analysis/analysis.tpl.html',
-//	    	controller: 'AnalysisCtrl',
-//	    	data : {
-//	    		activetab : 'analysis'
-//	    	}
-//	    })
-//	    .state('project', {
-//	    	url:'/projects',
-//	    	templateUrl : 'src/project/projectManagementPanel.html',
-//	    	controller : 'ProjectManagementCtrl',
-//	    	data: {
-//	    		activetab : 'project'
-//	    	}
-//	    })
-//	    .state('cross_tab',{
-//	    	url:'/cross_tab',
-//	    	templateUrl: 'src/analysis/crosstab/cross_tab.tpl.html',
-//	    	data :{
-//    			activetab : 'cross_tab'
-//    		}
-//	    })
-//	    .state('data_stats',{
-//	    	url:'/dataStatistics',
-//	    	templateUrl : 'src/dataStatistics/dataStatisticsMain.tpl.html',
-//    		controller : 'dataStatsCtrl',
-//    		data :{
-//    			activetab : 'data_stats'
-//    		}
-//	    })
-//	    .state('data_stats.summary_stats',{
-//	    	url:'/summary_stats',
-//	    	templateUrl: 'src/dataStatistics/summary_stats.tpl.html',
-//	    	data :{
-//    			activetab : 'summary_stats'
-//    		}
-//	    })
-//	    .state('data_stats.correlations', {
-//	    	url:'/correlations',
-//	    	templateUrl : 'src/dataStatistics/correlation_matrices.tpl.html',
-//	    	data :{
-//    			activetab : 'correlations'
-//    		}
-//	    })
-//	    .state('data_stats.regression', {
-//	    	url:'/regression',
-//	    	templateUrl :'src/dataStatistics/regression_analysis.tpl.html',
-//	    	data:{
-//	    		activetab: 'regression'
-//	    	}
-//	    });
-//		
-//	    
-//});
-//
+weave_Analyst.run(['$rootScope', function($rootScope){
+	$rootScope.$safeApply = function(fn, $scope) {
+			if($scope == undefined){
+				$scope = $rootScope;
+			}
+			fn = fn || function() {};
+			if ( !$scope.$$phase ) {
+        	$scope.$apply( fn );
+    	}
+    	else {
+        	fn();
+    	}
+	};
+}])
+.config(function($stateProvider, $urlRouterProvider) {
+	
+	//$parseProvider.unwrapPromises(true);
+	
+	$urlRouterProvider.otherwise('/index');
+	
+	$stateProvider
+		.state('index', {
+			url : '/index'
+		})
+		.state('metadata', {
+			url:'/metadata',
+			templateUrl : 'src/configure/metadata/metadataManager.html',
+			//controller: 'MetadataManagerCtrl',
+			data : {
+				activetab : 'metadata'
+			}
+		})
+	    .state('script_management', {
+	    	url:'/scripts',
+	    	templateUrl : 'src/configure/script/scriptManager.html',
+	    	//controller : 'ScriptManagerCtrl',
+	    	data:{
+	    		activetab : 'script_management'
+	    	}
+	    })
+	    .state('analysis', {
+	    	url:'/analysis',
+	    	templateUrl : 'src/analysis/analysis.tpl.html',
+	    	//controller: 'AnalysisCtrl',
+	    	data : {
+	    		activetab : 'analysis'
+	    	}
+	    })
+	    .state('project', {
+	    	url:'/projects',
+	    	templateUrl : 'src/project/projectManagementPanel.html',
+	    	//controller : 'ProjectManagementCtrl',
+	    	data: {
+	    		activetab : 'project'
+	    	}
+	    })
+	    .state('cross_tab',{
+	    	url:'/cross_tab',
+	    	templateUrl: 'src/analysis/crosstab/cross_tab.tpl.html',
+	    	data :{
+    			activetab : 'cross_tab'
+    		}
+	    })
+	    .state('data_stats',{
+	    	url:'/dataStatistics',
+	    	templateUrl : 'src/dataStatistics/dataStatisticsMain.tpl.html',
+    		//controller : 'dataStatsCtrl',
+    		data :{
+    			activetab : 'data_stats'
+    		}
+	    })
+	    .state('data_stats.summary_stats',{
+	    	url:'/summary_stats',
+	    	templateUrl: 'src/dataStatistics/summary_stats.tpl.html',
+	    	data :{
+    			activetab : 'summary_stats'
+    		}
+	    })
+	    .state('data_stats.correlations', {
+	    	url:'/correlations',
+	    	templateUrl : 'src/dataStatistics/correlation_matrices.tpl.html',
+	    	data :{
+    			activetab : 'correlations'
+    		}
+	    })
+	    .state('data_stats.regression', {
+	    	url:'/regression',
+	    	templateUrl :'src/dataStatistics/regression_analysis.tpl.html',
+	    	data:{
+	    		activetab: 'regression'
+	    	}
+	    });
+		
+	    
+});
+
 ///**********************Using ng-route***************************************/
 ////.config(function($parseProvider, $routeProvider){
 ////	$parseProvider.unwrapPromises(true);
@@ -169,7 +169,7 @@ var weave_Analyst = angular.module('weaveAnalyst',[]);
 //weave_Analyst.value('computationServiceURL', '/WeaveAnalystServices/ComputationalServlet');
 //weave_Analyst.value('WeaveDataSource', 'WeaveDataSource');
 //
-weave_Analyst.controller('AWSController', function() {
+weave_Analyst.controller('AWSController', function($scope, $state) {
 //
 //	 $scope.startSpin = function() {
 //    if (!$scope.spinneractive) {
@@ -194,7 +194,7 @@ weave_Analyst.controller('AWSController', function() {
 //    $scope.spinneractive = false;
 //  });
 //
-//	$scope.state = $state;
+	$scope.state = $state;
 //	$scope.authenticationService = authenticationService;
 //	
 //	$scope.$on('queryObjectloaded', function(event,incoming_queryObject){
