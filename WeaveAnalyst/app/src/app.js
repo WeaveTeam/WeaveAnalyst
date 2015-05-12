7342f356fa1c341518eab5f0b22a8710575b1236
shwetapurushe <shwetapurushe@gmail.com>
2015-05-13 03:21:42 +0530

    added more utility functions

--- WeaveAnalyst/app/src/app.js
+++ WeaveAnalyst/app/src/app.js
@@ -2,61 +2,150 @@
 //                                 'ui.select',
 //                                 'ui.sortable',
 
+//
+//
+//angular.module('weaveAnalyst').run(['$rootScope', function($rootScope){
+//	$rootScope.$safeApply = function(fn, $scope) {
+//			if($scope == undefined){
+//				$scope = $rootScope;
+//			}
+//			fn = fn || function() {};
+//			if ( !$scope.$$phase ) {
+//        	$scope.$apply( fn );
+//    	}
+//    	else {
+//        	fn();
+//    	}
+//	};
+//}])
+//.config(function($stateProvider, $urlRouterProvider) {
+//	
+//	//$parseProvider.unwrapPromises(true);
+//	
+//	$urlRouterProvider.otherwise('/index');
+//	
+//	$stateProvider
+//		.state('index', {
+//			url : '/index'
+//		})
+//		.state('metadata', {
+//			url:'/metadata',
+//			templateUrl : 'src/configure/metadata/metadataManager.html',
+//			//controller: 'MetadataManagerCtrl',
+//			data : {
+//				activetab : 'metadata'
+//			}
+//		})
+//	    .state('script_management', {
+//	    	url:'/scripts',
+//	    	templateUrl : 'src/configure/script/scriptManager.html',
+//	    	//controller : 'ScriptManagerCtrl',
+//	    	data:{
+//	    		activetab : 'script_management'
+//	    	}
+//	    })
+//	    .state('analysis', {
+//	    	url:'/analysis',
+//	    	templateUrl : 'src/analysis/analysis.tpl.html',
+//	    	//controller: 'AnalysisCtrl',
+//	    	data : {
+//	    		activetab : 'analysis'
+//	    	}
+//	    })
+//	    .state('project', {
+//	    	url:'/projects',
+//	    	templateUrl : 'src/project/projectManagementPanel.html',
+//	    	//controller : 'ProjectManagementCtrl',
+//	    	data: {
+//	    		activetab : 'project'
+//	    	}
+//	    })
+//	    .state('cross_tab',{
+//	    	url:'/cross_tab',
+//	    	templateUrl: 'src/analysis/crosstab/cross_tab.tpl.html',
+//	    	data :{
+//    			activetab : 'cross_tab'
+//    		}
+//	    })
+//	    .state('data_stats',{
+//	    	url:'/dataStatistics',
+//	    	templateUrl : 'src/dataStatistics/dataStatisticsMain.tpl.html',
+//    		//controller : 'dataStatsCtrl',
+//    		data :{
+//    			activetab : 'data_stats'
+//    		}
+//	    })
+//	    .state('data_stats.summary_stats',{
+//	    	url:'/summary_stats',
+//	    	templateUrl: 'src/dataStatistics/summary_stats.tpl.html',
+//	    	data :{
+//    			activetab : 'summary_stats'
+//    		}
+//	    })
+//	    .state('data_stats.correlations', {
+//	    	url:'/correlations',
+//	    	templateUrl : 'src/dataStatistics/correlation_matrices.tpl.html',
+//	    	data :{
+//    			activetab : 'correlations'
+//    		}
+//	    })
+//	    .state('data_stats.regression', {
+//	    	url:'/regression',
+//	    	templateUrl :'src/dataStatistics/regression_analysis.tpl.html',
+//	    	data:{
+//	    		activetab: 'regression'
+//	    	}
+//	    });
+//		
+//	    
+//});
 
-angular.module('weaveAnalyst',['ui.router',
-                               'ui.grid',
-                               'ui.bootstrap',
-                               'mk.editablespan',
-                               'ngAnimate',
-                               'mgcrea.ngStrap',
-                               'angularSpinner',
-                               'ngSanitize',
-                               'weaveAnalyst.configure',
-                               'weaveAnalyst.dataStatistics',
-                               'weaveAnalyst.directives',
-                               'weaveAnalyst.queryObject',                             
-                               'weaveAnalyst.queryObjectEditor', 
-                               'weaveAnalyst.project',
-                               'weaveAnalyst.errorLog',
-                               'weaveAnalyst.AnalysisModule',
-                               'weaveAnalyst.WeaveModule',
-                               'weaveAnalyst.QueryHandlerModule']);
+//Using IIFEs
+(function($stateProvider, $urlRouterProvider, $rootScope){
 
-angular.module('weaveAnalyst.configure', ['weaveAnalyst.configure.auth',
-                                          'weaveAnalyst.configure.metadata',
-                                          'weaveAnalyst.configure.script']);
+	angular.module('weaveAnalyst',['ui.router',
+	                               'ui.grid',
+	                               'ui.bootstrap',
+	                               'mk.editablespan',
+	                               'ngAnimate',
+	                               'mgcrea.ngStrap',
+	                               'angularSpinner',
+	                               'ngSanitize',
+	                               'weaveAnalyst.configure',
+	                               'weaveAnalyst.dataStatistics',
+	                               'weaveAnalyst.directives',
+	                               'weaveAnalyst.queryObject',                             
+	                               'weaveAnalyst.queryObjectEditor', 
+	                               'weaveAnalyst.project',
+	                               'weaveAnalyst.errorLog',
+	                               'weaveAnalyst.AnalysisModule',
+	                               'weaveAnalyst.WeaveModule',
+	                               'weaveAnalyst.QueryHandlerModule']);
 
-
-angular.module('weaveAnalyst.directives', ['weaveAnalyst.directives.dualListBox',
-                                'weaveAnalyst.directives.fileUpload',
-                                'weaveAnalyst.directives.popover-with-tpl']);
-
-
-
-//using the value provider recipe 
-angular.module('weaveAnalyst').value("dataServiceURL", '/WeaveServices/DataService');
-angular.module('weaveAnalyst').value('adminServiceURL', '/WeaveServices/AdminService');
-angular.module('weaveAnalyst').value('projectManagementURL', '/WeaveAnalystServices/ProjectManagementServlet');
-angular.module('weaveAnalyst').value('scriptManagementURL', '/WeaveAnalystServices/ScriptManagementServlet');
-angular.module('weaveAnalyst').value('computationServiceURL', '/WeaveAnalystServices/ComputationalServlet');
-angular.module('weaveAnalyst').value('WeaveDataSource', 'WeaveDataSource');
-
-
-angular.module('weaveAnalyst').run(['$rootScope', function($rootScope){
-	$rootScope.$safeApply = function(fn, $scope) {
-			if($scope == undefined){
-				$scope = $rootScope;
-			}
-			fn = fn || function() {};
-			if ( !$scope.$$phase ) {
-        	$scope.$apply( fn );
-    	}
-    	else {
-        	fn();
-    	}
-	};
-}])
-.config(function($stateProvider, $urlRouterProvider) {
+	
+	angular.module('weaveAnalyst.configure', ['weaveAnalyst.configure.auth',
+	                                          'weaveAnalyst.configure.metadata',
+	                                          'weaveAnalyst.configure.script']);
+	angular.module('weaveAnalyst.directives', ['weaveAnalyst.directives.dualListBox',
+	                                'weaveAnalyst.directives.fileUpload',
+	                                'weaveAnalyst.directives.popover-with-tpl']);
+	
+	
+	angular.module('weaveAnalyst').run(['$rootScope', function($rootScope){
+		$rootScope.$safeApply = function(fn, $scope) {
+				if($scope == undefined){
+					$scope = $rootScope;
+				}
+				fn = fn || function() {};
+				if ( !$scope.$$phase ) {
+	        	$scope.$apply( fn );
+	    	}
+	    	else {
+	        	fn();
+	    	}
+		};
+	}]);
+	angular.module('weaveAnalyst').config(function($stateProvider, $urlRouterProvider) {
 	
 	//$parseProvider.unwrapPromises(true);
 	
@@ -69,7 +158,8 @@ angular.module('weaveAnalyst').run(['$rootScope', function($rootScope){
 		.state('metadata', {
 			url:'/metadata',
 			templateUrl : 'src/configure/metadata/metadataManager.html',
-			//controller: 'MetadataManagerCtrl',
+			controller: 'MetadataManagerController',
+			controllerAs : 'mDataCtrl',
 			data : {
 				activetab : 'metadata'
 			}
@@ -137,10 +227,29 @@ angular.module('weaveAnalyst').run(['$rootScope', function($rootScope){
 		
 	    
 });
-
-angular.module('weaveAnalyst').controller('weaveAnalystController', function($scope,$rootScope, $state, authenticationService,usSpinnerService, queryService, WeaveService) {
 	
-	this.state = $state;
-	this.authenticationService = authenticationService;
+	angular.module('weaveAnalyst').controller('weaveAnalystController',weaveAnalystController );
+	
+	weaveAnalystController.$inject = ['$state','authenticationService'];
+	function weaveAnalystController ($state,authenticationService){//treating controllers as a constructor
+		
+		var vm = this;
+		
+		vm.state = $state;
+		vm.authenticationService = authenticationService;
+	};
+	
+	
+	//using the value provider recipe 
+	angular.module('weaveAnalyst').value("dataServiceURL", '/WeaveServices/DataService');
+	angular.module('weaveAnalyst').value('adminServiceURL', '/WeaveServices/AdminService');
+	angular.module('weaveAnalyst').value('projectManagementURL', '/WeaveAnalystServices/ProjectManagementServlet');
+	angular.module('weaveAnalyst').value('scriptManagementURL', '/WeaveAnalystServices/ScriptManagementServlet');
+	angular.module('weaveAnalyst').value('computationServiceURL', '/WeaveAnalystServices/ComputationalServlet');
+	angular.module('weaveAnalyst').value('WeaveDataSource', 'WeaveDataSource');
 
-});
+
+
+})();
+
+
--- WeaveAnalyst/app/src/configure/metadata/metadataManagerService.js
+++ WeaveAnalyst/app/src/configure/metadata/metadataManagerService.js
@@ -1,73 +1,95 @@
-angular.module('weaveAnalyst.configure.metadata').service("metadataService", ['$q', '$rootScope','queryService', 'authenticationService',
-                                           function($q, rootScope, queryService, authenticationService) {
-	this.gridData = [];
-	var that = this;
+(function (){
 	
-	/**
-	  * this function is called whenever the user adds or deletes a column metadata property
-	  * function converts an object into a json string to send to server
-	  */
-	 this.updateMetadata = function(metadata) {
-		 var jsonaws_metadata = angular.toJson(convertToMetadataFormat(metadata));
-		 if(angular.isDefined($scope.selectedDataTableId))
-			 {
-						 queryService.updateEntity($scope.authenticationService.user, 
-				 				   $scope.authenticationService.password, 
-				 				   $scope.selectedDataTableId, 
-				 				   { 
-										publicMetadata : { aws_metadata : jsonaws_metadata }
-				 				   }).then(function() {
-				 					   	//$scope.maxTasks = 100;
-				 					   //$scope.progressValue = 100;
-				 				   });
-			 }
-		 
-		 alert("Metadata Updated for id : " + $scope.selectedDataTableId);
-	 };
- 
-	 this.setGridData = function(data) {
-		  this.gridData = data;
-		  rootScope.$safeApply();
-	 };
-	 
-	 
-	/**
-	 * function that converts a aws-metadata json object into an array of objects that look like this { property:
-	 * 																	 								value : }
-	 * for using in the grid
-	 * */
-	this.convertToTableFormat = function(aws_metadata) {
-		var data = [];
-		for (var key in aws_metadata) {
-			data.push({property : key, value : angular.toJson(aws_metadata[key]) });
-		}
-		return data;
-	};
+	angular.module('weaveAnalyst.configure.metadata').service('metadataService', metadataService);
+	
+	metadataService.$inject = ['$q', '$rootScope','queryService', 'authenticationService'];
+	
+	function metadataService($q, rootScope, queryService, authenticationService){
 		
-	/**
-	 * retrieves the metadata for a single column
-	 * */
-	this.getColumnMetadata = function (columnObject) {
-		if(columnObject && columnObject.id) {
-			queryService.getEntitiesById([columnObject.id], true).then(function(entity) {
-					entity = entity[0];
-					if(entity.publicMetadata.hasOwnProperty('aws_metadata')) {
-						var data = [];
-						var aws_metadata = angular.fromJson(entity.publicMetadata.aws_metadata);//converts the json string into an object
-						data = that.convertToTableFormat(aws_metadata);//to use in the grid
-						that.setGridData(data);
-					}
-			});
-		} else {
-				this.setGridData([]);
-		}
-	};
-
-	this.cmp = function(a, b) {
-		key1 = a.data.key;
-		key2 = b.data.key;
-		return key1 > key2 ? 1 : key1 < key2 ? -1 : 0;
-	};
+		var that = this;
+		that.gridData = [];
+		
+		/**
+		  * this function is called whenever the user adds or deletes a column metadata property
+		  * function converts an object into a json string to send to server
+		  */
+		 that.updateMetadata = function(metadata) {
+			 var jsonaws_metadata = angular.toJson(that.convertToMetadataFormat(metadata));
+			 if(angular.isDefined($scope.selectedDataTableId))
+				 {
+							 queryService.updateEntity($scope.authenticationService.user, 
+					 				   $scope.authenticationService.password, 
+					 				   $scope.selectedDataTableId, 
+					 				   { 
+											publicMetadata : { aws_metadata : jsonaws_metadata }
+					 				   }).then(function() {
+					 					   	//$scope.maxTasks = 100;
+					 					   //$scope.progressValue = 100;
+					 				   });
+				 }
+			 
+			 alert("Metadata Updated for id : " + $scope.selectedDataTableId);
+		 };
 	 
-    
-}]);
+		 that.setGridData = function(data) {
+			  that.gridData = data;
+			  rootScope.$safeApply();
+		 };
+		 
+		 
+		/**
+		 * function that converts a aws-metadata json object into an array of objects that look like this { property:
+		 * 																	 								value : }
+		 * for using in the grid
+		 * */
+		that.convertToTableFormat = function(aws_metadata) {
+			var data = [];
+			for (var key in aws_metadata) {
+				data.push({property : key, value : angular.toJson(aws_metadata[key]) });
+			}
+			return data;
+		};
+		
+
+
+		/**
+		* function that converts a object { property: , value : } into an aws_metadata json object
+		* for updating to the server
+		* */
+		that.convertToMetadataFormat = function (tableData) {
+			var aws_metadata = {};
+			for (var i in tableData) {
+			aws_metadata[tableData[i].property] = angular.fromJson(tableData[i].value);
+			}
+			return aws_metadata;
+		};
+
+
+			
+		/**
+		 * retrieves the metadata for a single column
+		 * */
+		that.getColumnMetadata = function (columnObject) {
+			if(columnObject && columnObject.id) {
+				queryService.getEntitiesById([columnObject.id], true).then(function(entity) {
+						entity = entity[0];
+						if(entity.publicMetadata.hasOwnProperty('aws_metadata')) {
+							var data = [];
+							var aws_metadata = angular.fromJson(entity.publicMetadata.aws_metadata);//converts the json string into an object
+							data = that.convertToTableFormat(aws_metadata);//to use in the grid
+							that.setGridData(data);
+						}
+				});
+			} else {
+					that.setGridData([]);
+			}
+		};
+
+		that.cmp = function(a, b) {
+			key1 = a.data.key;
+			key2 = b.data.key;
+			return key1 > key2 ? 1 : key1 < key2 ? -1 : 0;
+		};		
+	};
+	
+})();//end of IIFE
