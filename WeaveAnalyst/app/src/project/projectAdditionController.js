4a4cc608eb4e330ae51e80bb8a0f5f111ca2dd86
shwetapurushe <shwetapurushe@gmail.com>
2015-05-17 00:54:47 +0530

    fixed modal.open in prjt(work in progress)

--- WeaveAnalyst/app/src/project/projectAdditionController.js
+++ WeaveAnalyst/app/src/project/projectAdditionController.js
@@ -1,9 +1,51 @@
+(function(){
+	angular.module('weaveAnalyst.project').controller("projectAdditionController",projectAdditionController);
+	
+	projectAdditionController.$inject = ['$scope', '$modal', 'projectService'];
+	function projectAdditionController ($scope,$modal, projectService){
+		var prAddCtrl = this;
+		
+		prAddCtrl.openAdditionPanel = openAdditionPanel;
+		//options needed for creating the modal instance window
+		 //communicating with the modal
+		prAddCtrl.pjtModalOptions = pjtModalOptions = {//TODO find out how to push error log to bottom of page
+				 backdrop: true,
+		         backdropClick: false,
+		         dialogFade: true,
+		         keyboard: true,
+		         templateUrl: 'src/project/projectAdditionPanel.html',
+		         controller: 'pjtAddtionInstanceController',
+		         resolve :{
+		        	 projectNameEntered : function(){return $scope.projectNameEntered;},
+		        	 userNameEntered : function(){return $scope.userNameEntered;},
+		        	 projectDescriptionEntered : function(){return $scope.projectDescriptionEntered;}
+		        	 
+		         }
+			};
+		
+		//button click event that creates the modal
+		function openAdditionPanel(){
+			var saveNewProject = $modal.open(prAddCtrl.pjtModalOptions);
+			
+			//called when modal is being closed
+			saveNewProject.result.then(function(additionParams){//then function takes a single object
+				 console.log("jsons", additionParams.uploadedObjects.queryObjectJsons);
+				 console.log("titles", additionParams.uploadedObjects.queryObjectTitles);
+				 console.log("userName", additionParams.userNameEntered);
+				 
+				 
+				 
+			});
+		};
+	};
+	
 
-angular.module('weaveAnalyst.project').controller("projectAdditionController", function(){
-
-});
-
-//Modal instance controller
-angular.module('weaveAnalyst.project').controller('pjtAddtionInstanceCtrl', function() {
-
-});
+	//Modal instance controller
+	angular.module('weaveAnalyst.project').controller('pjtAddtionInstanceController', additionController);
+	additionController.$inject= ['$modalInstance','projectService', 'projectNameEntered','projectDescriptionEntered', 'userNameEntered'];
+	
+	function additionController ($modalInstance,projectService, projectNameEntered,projectDescriptionEntered, userNameEntered){
+		
+	};
+	
+})();
