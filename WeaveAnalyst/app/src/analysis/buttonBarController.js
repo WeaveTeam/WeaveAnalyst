9ce56e51662aa0477affe8fb3a07b7bc66fe8ab6
shwetapurushe <shwetapurushe@gmail.com>
2015-05-19 08:05:31 +0530

    moving button bar controller to analysis from qo

---
+++ WeaveAnalyst/app/src/analysis/buttonBarController.js
@@ -0,0 +1,11 @@
+/**
+ * buttonBarController. This controller manages query import and exports.
+ */
+
+angular.module("weaveAnalyst.AnalysisModule").controller("buttonBarController", function() {
+	
+});
+
+angular.module("weaveAnalyst.AnalysisModule").controller('DialogInstanceCtrl', function () {
+	  
+});
