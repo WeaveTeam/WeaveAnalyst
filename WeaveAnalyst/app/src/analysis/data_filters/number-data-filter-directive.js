316505c4903f3562d0e33c7a2e750a02fc8f284c
franckamayou <franckamayou@gmail.com>
2015-06-12 17:14:00 -0400

    fixed callback issue in number data filter

--- WeaveAnalyst/app/src/analysis/data_filters/number-data-filter-directive.js
+++ WeaveAnalyst/app/src/analysis/data_filters/number-data-filter-directive.js
@@ -28,27 +28,30 @@ AnalysisModule.directive('numberDataFilter', function(WeaveService) {
 			var pathToFilters = ["defaultSubsetKeyFilter", "filters"];
 			
 			var filterName = $scope.$parent.filterName;
-
+			
+			$scope.ngModel.range = [];
+			
 			$scope.$watch('ngModel.column', function(column) {
 				
 				var weave = WeaveService.weave;
 				if(column)
 				{
+					$scope.ngModel.range = [];
 					if(weave && WeaveService.checkWeaveReady()) {
 						weave.path(pathToFilters).push(filterName, "column").setColumn(column.metadata, column.dataSourceName);
 
-						weave.path(pathToFilters).push(filterName).addCallback(function() {
+						weave.path(pathToFilters).push(filterName).exec("registerLinkableChild(this, WeaveAPI.StatisticsCache.getColumnStatistics(column))").addCallback(function() {
 							var min = this.getValue("EquationColumnLib.getMin(column);") || 0;
 							var max = this.getValue("EquationColumnLib.getMax(column);") || 100;
+							// default handle locations
 							$scope.sliderOptions = { range:true, min:min, max:max };
-							$scope.$apply();
+							$rootScope.$safeApply();
 						});
 					}
 				} 
 			}, true);
 
 			$scope.$watchCollection('ngModel.range', function() {
-				console.log($scope.ngModel.range);
 				weave.path(pathToFilters).push(filterName, "min").state($scope.ngModel.range[0]);
 				weave.path(pathToFilters).push(filterName, "max").state($scope.ngModel.range[1]);
 			});
