b3e7957ac8bc2c865525161dd1bc4aa51268369d
shwetapurushe <shwetapurushe@gmail.com>
2015-07-17 18:57:59 -0400

    scatter plot widget start

--- WeaveAnalyst/app/src/visualization/weave/tools/scatterPlot/scatter_plot_directive.js
+++ WeaveAnalyst/app/src/visualization/weave/tools/scatterPlot/scatter_plot_directive.js
@@ -4,10 +4,31 @@
 
 (function(){
 	
-	angular.module('weaveApp').controller('scatterPlotController', scatterPlotController );
+	angular.module('weaveApp').directive('scatterPlot', scatterPlot);
 	
-	function scatterPlotController(){
+	function scatterPlot (){
+		return {
+			restrict: 'E',
+			scope:{},
+			templateUrl:'tools/scatterPlot/scatter_plot.tpl.html', 
+			controller : scatter_plotController,
+			controllerAs : 'spCtrl',
+			bindToController: true,
+			link: function(){
+				
+			}
+		};//directive definition object
+	}
+	
+	function scatter_plotController (){
+		var spCtrl = this;
 		
+		spCtrl.items = ['a','d'];
+		
+		spCtrl.config = {
+			X : null,
+			Y : null
+		};
 	};
 	
 })();
