2a82fcbe9d4e26c2616a7dacff9c5d972b131179
shwetapurushe <shwetapurushe@gmail.com>
2015-07-17 20:21:12 -0400

    made checkWeaveReady static and added instance property

--- WeaveAnalyst/app/src/visualization/weave/WeaveWrapper.js
+++ WeaveAnalyst/app/src/visualization/weave/WeaveWrapper.js
@@ -8,18 +8,28 @@ if(!this.wa)
 
 (function(){
 	function WeaveWrapper (){
-		this.weave = document.getElementById('weave');
+		//TODO move this into a manager class
+		WeaveWrapper.instance = this;
+		
 	}
 	
+	WeaveWrapper.check_WeaveReady = function(){
+		console.log("this",this);
+		WeaveWrapper.weave = document.getElementById('weave');
+		return WeaveWrapper.weave && WeaveWrapper.weave.WeavePath && WeaveWrapper.weave._jsonCall;
+	};
+	
 	var p = WeaveWrapper.prototype;
+	//attaching it to the global wa object
+	wa.WeaveWrapper = WeaveWrapper;
 	
 	//////////////
 	////VIZs
 	//////////////
 	p.request_BarChart = function(tool_config){
-		if(this.check_WeaveReady()){
+		if(WeaveWrapper.check_WeaveReady()){
 			var toolName = this.generate_UniqueName("BarChartTool");
-			this.weave.path(toolName)
+			WeaveWrapper.weave.path(toolName)
 			.request('CompoundBarChartTool')
 			.tool_config({  showAllLabels : tool_config.showAllLabels })
 			.push('children', 'visualization', 'plotManager', 'plotters', 'plot')
@@ -49,11 +59,14 @@ if(!this.wa)
 	
 	p.request_ScatterPlot = function(tool_config){
 		var toolName;
-		if(this.check_WeaveReady()){
+		if(WeaveWrapper.check_WeaveReady()){
 			
-			toolName = this.generate_UniqueName("ScatterPlotTool");
+			if(tool_config.toolName)
+				toolName = tool_config.toolName;
+			else
+				toolName = this.generate_UniqueName("ScatterPlotTool");
 			
-			 this.weave.path(toolName).request('ScatterPlotTool')
+			 WeaveWrapper.weave.path(toolName).request('ScatterPlotTool')
 			.push('children', 'visualization','plotManager', 'plotters', 'plot')
 			.push('dataX').setColumn(tool_config.X.metadata, tool_config.X.dataSourceName)
 			.pop()
@@ -70,11 +83,11 @@ if(!this.wa)
 	
 	p.request_AdvancedDataTable = function(tool_config){
 		var toolName;
-		if (this.check_WeaveReady())
+		if (WeaveWrapper.check_WeaveReady())
 		{
 			toolName = this.generate_UniqueName("DataTableTool");
 			
-			this.weave.path(toolName).request('AdvancedTableTool')
+			WeaveWrapper.weave.path(toolName).request('AdvancedTableTool')
 			.push("columns").setColumns(tool_config && tool_config.columns && tool_config.columns.length ? tool_config.columns.map(function(column) {
 				return column.metadata;
 			}) : {}, tool_config && tool_config.columns && tool_config.columns[0] ? tool_config.columns[0].dataSourceName : ""); 
@@ -101,7 +114,7 @@ if(!this.wa)
 	///////////////
 	p.request_AttributeMenu = function(){
 		var toolName;
-		if(this.check_WeaveReady()){
+		if(WeaveWrapper.check_WeaveReady()){
 			toolName = aToolName || ws.generateUniqueName("AttributeMenuTool");
 			ws.weave.path(toolName).request('AttributeMenuTool').call(setQueryColumns, {choices: tool_config.columns});
 				
@@ -117,21 +130,21 @@ if(!this.wa)
 	
 	p.request_DataFilter = function(){
 		
-		if(this.check_WeaveReady()){
+		if(WeaveWrapper.check_WeaveReady()){
 			
-			this.weave.path(toolName).request('DataFilterTool');
+			WeaveWrapper.weave.path(toolName).request('DataFilterTool');
 			
 			if(tool_config.filterStyle == "Discrete values") {
-				this.weave.path(toolName, "editor", null).request("StringDataFilterEditor").tool_config({
+				WeaveWrapper.weave.path(toolName, "editor", null).request("StringDataFilterEditor").tool_config({
 					layoutMode : tool_config.layoutMode.value,
 					showPlayButton : tool_config.showPlayButton,
 					showToggle : tool_config.showToggle
 				});
 			} else if(tool_config.filterStyle == "Continuous range") {
-				this.weave.path(toolName, "editor", null).request("NumberDataFilterEditor");
+				WeaveWrapper.weave.path(toolName, "editor", null).request("NumberDataFilterEditor");
 			}
 			if(tool_config.column) {
-				this.weave.path(toolName, "filter", null, "column").setColumn(tool_config.column.metadata, tool_config.column.dataSourceName);
+				WeaveWrapper.weave.path(toolName, "filter", null, "column").setColumn(tool_config.column.metadata, tool_config.column.dataSourceName);
 			}
 		}
 		else{
@@ -147,7 +160,7 @@ if(!this.wa)
 	///COLOR///////
 	
 	p.set_ColorGroup = function(toolName, plotName, groupName, column){
-		var plotterPath = this.weave.path(toolName).pushPlotter(plotName);
+		var plotterPath = WeaveWrapper.weave.path(toolName).pushPlotter(plotName);
 		var plotType = plotterPath.getType();
 		if (!plotName) plotName = "plot";
 		var dynamicColumnPath;
@@ -162,11 +175,11 @@ if(!this.wa)
 		}
 		
 		dynamicColumnPath.vars({name: groupName}).getValue("ColumnUtils.unlinkNestedColumns(this); globalName = name");
-		this.weave.path(groupName).getValue("ColumnUtils.hack_findInternalDynamicColumn(this)").setColumn(column.metadata, column.dataSourceName);
+		WeaveWrapper.weave.path(groupName).getValue("ColumnUtils.hack_findInternalDynamicColumn(this)").setColumn(column.metadata, column.dataSourceName);
 	};
 	
 	p.get_ColorGroups = function(){
-		return	this.weave.path().getValue('getNames(ColorColumn)');
+		return	WeaveWrapper.weave.path().getValue('getNames(ColorColumn)');
 	};
 	
 	/////////////
@@ -175,7 +188,7 @@ if(!this.wa)
 	
 	p.get_base64_SessionState = function(){
 		
-		return this.weave.path().getValue("\
+		return WeaveWrapper.weave.path().getValue("\
 		        var e = new 'mx.utils.Base64Encoder'();\
 		        e.encodeBytes( Class('weave.Weave').createWeaveFileContent(true) );\
 		        return e.drain();\
@@ -184,7 +197,7 @@ if(!this.wa)
 	
 	p.set_base64_SessionState = function(){
 		
-		this.weave.path()
+		WeaveWrapper.weave.path()
 		.vars({encoded: base64encodedstring})
 		.getValue("\
 	        var d = new 'mx.utils.Base64Decoder'();\
@@ -195,30 +208,27 @@ if(!this.wa)
 	};
 	
 	p.clear_SessionState = function(){
-		this.weave.path().state(['WeaveDataSource']);
+		WeaveWrapper.weave.path().state(['WeaveDataSource']);
 	};
 	
-	p.check_WeaveReady = function(){
-		console.log("this",this);
-		return this.weave && this.weave.WeavePath && this.weave._jsonCall;
-	};
+	
 	
 	p.generate_UniqueName = function(className, path) {
-		if(!this.weave)
+		if(!WeaveWrapper.weave)
 			return null;
-		return this.weave.path(path || []).getValue('generateUniqueName')(className);
+		return WeaveWrapper.weave.path(path || []).getValue('generateUniqueName')(className);
 	};
 	
 	p.get_PathToFilters = function() {
 		if(!this.checkWeaveReady())
 			return;
-		return this.weave.path("scriptKeyFilter").request("KeyFilter").push("filters");//references the Linkableashmap 'filters' in a keyFilter
+		return WeaveWrapper.weave.path("scriptKeyFilter").request("KeyFilter").push("filters");//references the Linkableashmap 'filters' in a keyFilter
 	};
 	
 	p.tile_Windows = function() {
 		if(!this.checkWeaveReady())
 			return;
-		this.weave.path()
+		WeaveWrapper.weave.path()
 		.libs("weave.ui.DraggablePanel")
 		.exec("DraggablePanel.tileWindows()");
 	};
@@ -232,11 +242,7 @@ if(!this.wa)
 	};
 	
 	p.remove_Object = function(object_name){
-		this.weave.path(object_name).remove();
+		WeaveWrapper.weave.path(object_name).remove();
 	};
-	
-	
-	
-	//attaching it to the global wa object
-	wa.WeaveWrapper = WeaveWrapper;
+
 })();
