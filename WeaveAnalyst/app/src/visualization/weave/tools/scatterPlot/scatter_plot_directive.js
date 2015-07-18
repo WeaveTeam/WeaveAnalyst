4770523b6be780ad1bfba861339ecb63fc8e04e9
shwetapurushe <shwetapurushe@gmail.com>
2015-07-17 20:21:53 -0400

    requested scatter plot using config object

--- WeaveAnalyst/app/src/visualization/weave/tools/scatterPlot/scatter_plot_directive.js
+++ WeaveAnalyst/app/src/visualization/weave/tools/scatterPlot/scatter_plot_directive.js
@@ -22,12 +22,38 @@
 	
 	function scatter_plotController (){
 		var spCtrl = this;
+		var weave_wrapper;
 		
+		spCtrl.request_scatterPlot = request_scatterPlot;
 		spCtrl.items = ['a','d'];
 		
 		spCtrl.config = {
+			toolName: null,
 			X : null,
 			Y : null
+		};
+		
+		
+		function request_scatterPlot (){
+			if(wa.WeaveWrapper.check_WeaveReady()){//TODO figure out where to call checkWeaveReady
+				//TODO put this retrieval in manager class later
+				if(!wa.wWrapper)
+					weave_wrapper = new wa.WeaveWrapper();
+				else
+					weave_wrapper = WeaveWrapper.instance;
+				
+				
+				if(spCtrl.checked)//if checked
+					spCtrl.config.toolName = weave_wrapper.request_ScatterPlot(spCtrl.config);
+				else{//if unchecked
+					if(spCtrl.config.toolName)//if the tool exists
+						weave_wrapper.remove_Object(spCtrl.config.toolName);//remove it
+					else
+						return;
+				}
+			}
+			else
+				setTimeout(request_scatterPlot, 100);
 		};
 	};
 	
