e8e6f043b2de21ab73b21483d86272eb594ffdbb
shwetapurushe <shwetapurushe@gmail.com>
2015-07-22 16:39:05 -0400

    d3Service no longer needed

--- WeaveAnalyst/app/src/dataStatistics/d3Service.js
+++
@@ -1,7 +0,0 @@
-/**
- *this service contains all the functions needed for D3 visualizations 
- *TODO move to an independent D3 module later 
- */
-angular.module('weaveAnalyst.dataStatistics').service('d3Service', [function(){
-		
-}]);
