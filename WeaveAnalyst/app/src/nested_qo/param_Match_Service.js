e6cf6399f90f3f54dec5fbb8cc918c494edb39bb
shwetapurushe <shwetapurushe@gmail.com>
2015-09-01 16:26:20 -0400

    new service for nested qo construction

---
+++ WeaveAnalyst/app/src/nested_qo/param_Match_Service.js
@@ -0,0 +1,11 @@
+/**
+ *this service handles the validation and functions associated with executing a nested query object
+ *@author shweta purushe 
+ */
+(function(){
+	angular.module('weaveAnalyst.nested_qo').service('paramMatchService', paramMatchService);
+	
+	function paramMatchService (){
+		var that = this;
+	};
+})();
